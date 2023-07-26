0a4e3b0a79b93cd79095cd87d1e8a5f7ca992cd1
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2023-07-26 19:43:51 -0300

    feat add header menu

---
+++ my-app/src/components/Headermenu.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.scss';
+
+const Headermenu = () => {
+  return (
+    <header className='header'>
+        <ul>
+            <li>Organização</li>
+            <li>Tarefas</li>
+        </ul>
+    </header>
+  )
+}
+
+export default Headermenu
--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import './index.scss'
+import Headermenu from './components/Headermenu';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: < Headermenu />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
