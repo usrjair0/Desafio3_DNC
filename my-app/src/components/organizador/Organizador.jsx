0cf4eec91efd4cae27cc03fd16bb48b29418e144
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2023-07-26 22:38:57 -0300

    feat add images

--- my-app/src/components/organizador/Organizador.jsx
+++ my-app/src/components/organizador/Organizador.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './index.scss';
+import pencil from '../../assets/pencil.svg';
+import trash from '../../assets/trash.svg';
+import square from '../../assets/square.svg';
+import some from '../../assets/some.svg';
+import squareClicked from '../../assets/squareClicked.svg'
 
 const Organizador = ({data}) => {
+    //https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array
+    const [taskImages, setTaskImages] = useState(Array(data.length).fill(false)); //o fill preenche tudo com false.
+    const handleImageClick = (index) => {
+    const newTaskImages = [...taskImages];
+    newTaskImages[index] = !newTaskImages[index];
+    setTaskImages(newTaskImages);
+  };
   return (
     <main className='main'>
         <h2>Otimize seu tempo e se organize com o nosso Planejador Diário.</h2>
@@ -15,11 +27,27 @@ const Organizador = ({data}) => {
             {data.map((task, index) => (
                 <tr key={index}>
                     <td>{task.description}</td>
-                    <td></td> 
-                    <td></td>
+                    <td>
+                        {isSquareClicked ? (
+                            <img src={squareClicked} onClick={handleisSquareClicked} width={15}/>
+                        ):(
+                            <img src={square} onClick={handleisSquareClicked} width={15}/> 
+                        )}
+                    </td> 
+                    <td>
+                        <img src={pencil} width={20}/>
+                        <img src={trash} width={20}/>
+                    </td>
                 </tr>
             ))}
             </tbody> 
+            <tfoot>
+                <tr>
+                    <td>Nova tarefa...</td>
+                    <td></td>
+                    <td><img src={some}/></td>
+                </tr>
+            </tfoot>
         </table>
     </main>
   )
