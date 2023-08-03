efd31262d2881d132ffb436a1665398a9c70f147
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2023-08-03 20:30:49 -0300

    remove home comment

--- my-app/src/views/Home/Home.jsx
+++ my-app/src/views/Home/Home.jsx
@@ -7,26 +7,6 @@ import CardMenutrash from "../../components/CardMenu2/CardMenutrash";
 
 // O parâmetro data criado aqui é passado para a main e recebe o valor do meu Tarefas.mock
 const Home = ({ data }) => {
-  {
-    /* Lógica de abertura ou não do meu card de exclusão/alteração */
-  }
-  {/* const { tarefaId } = useParams();
-  const selectedtarefa = data.find((tarefa) => tarefa.id == tarefaId);
-  
-  const [ispenOpen, setispenOpen] = useState(false);
-  const [istrashOpen, setistrashOpen] = useState(false);
-  const handdlepenOpen = () => {
-    setispenOpen(true);
-  };
-  const handdlepenClose = () => {
-    setispenOpen(false);
-  };
-  const handdletrashOpen = () => {
-    setistrashOpen(true);
-  };
-  const handdletrashClose = () => {
-    setistrashOpen(false);
-  };*/}
   return (
     <div className="home">
       <Headermenu />
