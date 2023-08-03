6612cef27727ad3bd54d93695abf2ef72a735168
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2023-08-03 17:31:37 -0300

    add changes

--- my-app/src/components/CardMenu2/CardMenutrash.jsx
+++ my-app/src/components/CardMenu2/CardMenutrash.jsx
@@ -3,16 +3,16 @@ import './index.scss';
 
 const CardMenutrash = ({ data, onClose }) => {
   return (
-    <div className='CardMenutrash'>
+    <div className='Menutrash'>
       <h2>Otimize seu tempo e se organize com o nosso Planejador Diário.</h2>
-        <div className='Card'>
-          <h2>Deseja excluir esse item?</h2>
-          <p>{data.description}oi</p>
-          <div className='Card__button'>
-            <button className='Card__button Card__button--blue' onClick={onClose}>Não</button>
-            <button className='Card__button Card__button--white'>Sim</button>
-          </div>
+      <div className='Card'>
+        <h2>Deseja excluir esse item?</h2>
+        <p>{data.description}oi</p>
+        <div className='Card__button'>
+          <button className='Card__button Card__button--blue' onClick={onClose}>Não</button>
+          <button className='Card__button Card__button--white'>Sim</button>
         </div>
+      </div>
     </div>
   )
 }
