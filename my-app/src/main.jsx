c236dbeb7d259bc229153183c7d3d82c58fb6cf4
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2023-08-18 19:46:38 -0300

    add some changes

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.scss'
 import Home from './views/Home/Home';
-import { TAREFAS } from '../src/mock/Tarefas.mock'
+import { TAREFAS } from '../src/mock/Tarefas.mock';
 import TasksPen from './views/TasksPen/TasksPen';
 import TasksTrash from './views/TasksTrash/TasksTrash';
 
